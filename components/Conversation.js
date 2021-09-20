3c6b8c9699db7f8641cb14d4718b5502115fc5da
Alexander Fok <foktmalexander@gmail.com>
2021-09-19 22:33:19 -0700

    Improve UI for conversation component

--- components/Conversation.js
+++ components/Conversation.js
@@ -1,14 +1,44 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default function Conversation() {
   return (   
-    <div className='container h-screen w-5/6 md:w-2/3 pt-2 flex flex-col mx-auto text-gray-400'>
-      <textarea readOnly={true} className='rounded-sm flex-grow resize-none border-2 border-gray-300 focus:outline-none' defaultValue={'(16:02) USER2 :\n\tHELLO WORLD'}></textarea>
-      <div className='flex flex-row'>
+    <div className='container w-5/6 sm:w-3/4 pt-6 pb-12 pr-12 md:pr-36 lg:pr-48 flex flex-col mx-auto'>
+      <ul className='rounded-md border-2 border-gray-300 flex-grow focus:outline-none px-4 py-4 overflow-hidden overflow-y-scroll'>
+      <li className='py-2 mb-3'>
+        <p className='mb-3'>Person 1 : </p>
+        <p className='rounded-md border border-gray-400 inline-block py-2 px-4'>
+          <span>HELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLD</span>
+        </p>
+      </li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>WORLD</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>HELLO</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>WORLD</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>HELLO</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>WORLD</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>HELLO</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>WORLD</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>HELLO</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>WORLD</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>HELLO</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>WORLD</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>HELLO</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>WORLD</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>HELLO</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>WORLD</span></p></li>
+        <li className='py-2 mb-3'><p className='mb-3'>Person 1 : </p><p className='rounded-md border border-gray-400 inline-block py-2 px-4'><span>HELLO</span></p></li>
+        
+      </ul>
+      <div className='flex flex-row text-gray-400'>
         <button className='rounded-sm px-2 py-1 hover:bg-gray-200'>File</button> 
         <button className='rounded-sm px-2 py-1 hover:bg-gray-200'>Settings</button>
         <button className='rounded-sm px-2 py-1 hover:bg-gray-200'>Emoji</button>
       </div>
-      <textarea className='rounded-sm resize-none border-2 border-gray-300 h-24 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-transparent'></textarea>
-      <button className='rounded-md self-end px-2 py-1 my-1 hover:bg-gray-200'>Send</button>
+      <div className='flex flex-row item-stretch'>
+        <textarea className='rounded-md resize-none w-11/12 border-2 border-gray-300 h-10 text-lg focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-transparent'></textarea>
+        <button className='rounded-md w-1/12 px-2 m-2 text-xl text-gray-500 hover:bg-gray-200 hover:text-indigo-500'>
+          <FontAwesomeIcon icon={['fas', 'paper-plane']}/>
+        </button>
+      </div>
     </div>
   )
 }
