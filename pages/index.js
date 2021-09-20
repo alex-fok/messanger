2e192758945f5afdb1a3eac5913ce61300884bc8
Alexander Fok <foktmalexander@gmail.com>
2021-09-19 22:29:56 -0700

    Add side nav and nav bar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Conversation from '../components/Conversation'
+import Navbar from '../components/Navbar'
+import SideNav from '../components/SideNav'
 
 export default function Home() {
   return (
-    <div className='h-full'>
-      <Conversation/>
+    <div className='flex flex-row'>
+      <div className='h-screen w-100 flex flex-grow flex-col'>
+        <Navbar />
+        <div className='flex flex-row h-5/6'>
+          <SideNav />
+          <Conversation/>
+        </div>
+      </div>
     </div>
   )
 }
