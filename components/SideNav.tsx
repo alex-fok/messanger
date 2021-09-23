59308fa1e6a7e1706f934cf29140d149d449e440
Alexander Fok <foktmalexander@gmail.com>
2021-09-22 18:56:54 -0700

    Change on sidenav spacing & style

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,6 +1,6 @@
 const SideNav = () => {
   return (
-    <nav className='w-44 ml-4 lg:ml-12 lg:px-8'>
+    <nav className='w-44 ml-4 lg:ml-12 px-2'>
     <ul className='mt-12  text-xl font-semibold'>
       <li className='text-gray-400 mb-5 hover:text-gray-800 cursor-pointer'><a>Contact</a></li>
       <li className='text-gray-400 mb-5 hover:text-gray-800 cursor-pointer'><a>Profile</a></li>
@@ -9,8 +9,8 @@ const SideNav = () => {
     <figure>
       <figcaption className='text-gray-400 text-base font-light mb-2'>Direct Messages</figcaption>
       <ul>
-        <li className='text-gray-600 text-sm px-2 mb-1 cursor-pointer'>Person 1</li>
-        <li className='text-gray-600 text-sm px-2 mb-1 cursor-pointer'>Person 1</li>
+        <li className='rounded-sm text-gray-600 text-sm px-2 mb-1 cursor-pointer hover:bg-gray-300'>Person 1</li>
+        <li className='rounded-sm text-gray-600 text-sm px-2 mb-1 cursor-pointer hover:bg-gray-300'>Person 1</li>
       </ul>
     </figure>
     </nav>
