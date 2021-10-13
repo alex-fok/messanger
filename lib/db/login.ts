82f69debf7842e6fa9a7d81aeec8b484383edc14
Alexander Fok <foktmalexander@gmail.com>
2021-10-13 04:18:15 -0700

    Add db logic for user authentication

---
+++ lib/db/login.ts
@@ -0,0 +1,22 @@
+import serializeToken from '../authentication/serializeToken'
+import mongodb from './mongodb'
+import crypto from 'crypto'
+
+const login = async (username: string, password: string):Promise<string | unknown> => {
+  const client = await mongodb
+  const db = client.db()
+  
+  return new Promise(async(res, rej) => {
+    const user = await db.collection('user').findOne({username: username})
+
+    if (!user) return rej('User not found')
+    const [salt, dbPass] = user.password.split('.')
+    
+    crypto.scrypt(password, salt, 64, (err, derivedKey) => {
+      if (err) return rej('Error in scrypting password')
+
+      derivedKey.toString('hex') === dbPass ? res(serializeToken({username})) : rej('Password not match')
+    })
+  })
+}
+export default login
