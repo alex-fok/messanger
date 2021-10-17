ff72c25de46b978020485fcc322b9eee5f63b683
Alexander Fok <foktmalexander@gmail.com>
2021-10-16 17:30:21 -0700

    Add promise to avoid stalled requests

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -2,11 +2,15 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import dbLogin from '../../lib/db/login'
 
 export default function login(req:NextApiRequest, res:NextApiResponse) {
-  dbLogin(req.body.username, req.body.password)
-  .then(jwt => {
-    res.json({ jwt })
-  })
-  .catch(error => {
-    res.status(401).end()
+  return new Promise<void> (resolve => {
+    dbLogin(req.body.username, req.body.password)
+    .then(jwt => {
+      res.json({ jwt })
+      resolve()
+    })
+    .catch(error => {
+      res.status(401).end()
+      resolve()
+    })
   })
 } 
