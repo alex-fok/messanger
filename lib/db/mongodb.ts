34bb390b5fb7c2ee98cf5745f6973f885446e93c
Alexander Fok <foktmalexander@gmail.com>
2021-10-16 19:33:09 -0700

    Log MONGODBURI

--- lib/db/mongodb.ts
+++ lib/db/mongodb.ts
@@ -3,13 +3,11 @@ const MONGODB_URI = process.env.MONGODB_URI
 const NODE_ENV = process.env.NODE_ENV
 
 if (!MONGODB_URI) throw new Error('Cannot find MONGODB_URI')
-
+console.log('MONGODB_URI: ' + MONGODB_URI)
 const client = new MongoClient(MONGODB_URI)
 
 let clientPromise = NODE_ENV === 'development'
   ? global._mongoClientPromise || (global._mongoClientPromise = client.connect())
-  : client.connect().catch(err => {
-      console.error('MONGODBURI: ' + MONGODB_URI + '\n' +err)
-  })
+  : client.connect()
 
 export default clientPromise
