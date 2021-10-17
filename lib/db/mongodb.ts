1fa7017460ae05ad0e0a3421fb12172b84c3a336
Alexander Fok <foktmalexander@gmail.com>
2021-10-16 19:57:10 -0700

    Remove Log MONGODB_URI

--- lib/db/mongodb.ts
+++ lib/db/mongodb.ts
@@ -3,7 +3,6 @@ const MONGODB_URI = process.env.MONGODB_URI
 const NODE_ENV = process.env.NODE_ENV
 
 if (!MONGODB_URI) throw new Error('Cannot find MONGODB_URI')
-console.log('MONGODB_URI: ' + MONGODB_URI)
 const client = new MongoClient(MONGODB_URI)
 
 let clientPromise = NODE_ENV === 'development'
