0c44260825a28a1766ad30eed16f354f564d00a2
alex-fok <foktmalexander@gmail.com>
2022-01-29 04:50:08 -0800

    Add useValidate hook

---
+++ lib/hooks/useValidate.ts
@@ -0,0 +1,11 @@
+import { Dispatch, SetStateAction, useState } from 'react'
+import validate, { Rule } from '../../utils/validateInput'
+
+const useValidate = (name: string, init: string, options: Rule[] ):
+[string, Dispatch<SetStateAction<string>>, boolean, string] => {
+  const [value, setVal] = useState(init)
+
+  return [value, setVal, ...validate({name, value}, options)]
+}
+
+export default useValidate
